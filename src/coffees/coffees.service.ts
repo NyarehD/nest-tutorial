7518dd727ced8dcb5a47b22596f9d33c7fda71ab
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-03 10:16:32 +0630

    14. Send User-friendly error messages

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable, NotFoundException } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -11,21 +11,32 @@ export class CoffeesService {
       flavors: ['chocolate', 'mint'],
     },
   ];
+
   findAll() {
     return this.coffees;
   }
+
   findOne(id: string) {
-    return this.coffees.find((item) => item.id === +id);
+    throw 'a random error';
+
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
   }
+
   create(createCoffeeDto: any) {
     this.coffees.push(createCoffeeDto);
   }
+
   update(id: string, updateCoffeeDto: any) {
     const existingCoffee = this.findOne(id);
     if (existingCoffee) {
       // update the existingCoffee
     }
   }
+
   remove(id: string) {
     const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
     if (coffeeIndex >= 0) {
