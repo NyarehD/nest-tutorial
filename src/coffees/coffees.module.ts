b07a0aec09b2ee7f35d3335261fa3f94e582a1c7
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-07 20:29:25 +0630

    38. Class Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,15 +4,23 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Flavor } from './entities/flavor.entity';
 import { Coffee } from './entities/coffee.entity';
 import { CoffeesService } from './coffees.service';
-import { COFFEE_BRANDS } from './coffees.constants';
 
-class MockCoffeesService {
+class ConfigService {
+}
 
+class DevelopmentConfigService {
+}
+
+class ProductionConfigService {
 }
 
 @Module({
   controllers: [CoffeesController],
-  providers: [CoffeesService, { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] }],
+  providers: [CoffeesService,
+    {
+      provide: ConfigService,
+      useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService,
+    }],
   imports: [TypeOrmModule.forFeature([Flavor, Coffee, Event])],
   exports: [CoffeesService],
 })
