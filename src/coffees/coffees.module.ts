62fc2099022bbe78d3d9810d3bac393e00d2a960
Nyareh David <70676816+NyarehD@users.noreply.github.com>
2022-09-07 20:34:20 +0630

    39. Factory Providers

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,25 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Flavor } from './entities/flavor.entity';
 import { Coffee } from './entities/coffee.entity';
 import { CoffeesService } from './coffees.service';
+import { COFFEE_BRANDS } from './coffees.constants';
 
-class ConfigService {
-}
-
-class DevelopmentConfigService {
-}
-
-class ProductionConfigService {
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    // Do something
+    return ['buddy brew', 'nescafe'];
+  }
 }
 
 @Module({
   controllers: [CoffeesController],
   providers: [CoffeesService,
+    CoffeeBrandsFactory,
     {
-      provide: ConfigService,
-      useClass: process.env.NODE_ENV === 'development' ? DevelopmentConfigService : ProductionConfigService,
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
     }],
   imports: [TypeOrmModule.forFeature([Flavor, Coffee, Event])],
   exports: [CoffeesService],
